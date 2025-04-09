d076889d730d767181b822f273ecf3117d88a832
SayaKurma <ridzi1277@gmail.com>
2025-04-09 22:06:35 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,27 +1,8 @@
 const translations = [
-    {
-        id: "Selamat pagi",
-        en: "Good morning",
-        jp: "おはようございます (Ohayou gozaimasu)",
-    },
-    {
-        id: "Terima kasih banyak",
-        en: "Thank you very much",
-        jp: "ありがとうございます (Arigatou gozaimasu)",
-    },
-    {
-        id: "Terima kasih",
-        en: "Thank you",
-        jp: "ありがとう (Arigatou)",
-    },
-    {
-        id: "Toilet ada di sebelah mana?",
-        en: "Where's the toilet?",
-        jp: "トイレはどこ？ (Toire wa doko?)",
-    },
-    {
-        id: "Aku tidak paham",
-        en: "I don't understand",
-        jp: "わかりません (Wakarimasen)",
-    },
+    { id: "Selamat pagi", en: "Good morning", jp: "おはようございます<br>(Ohayou gozaimasu)" },
+    { id: "Terima kasih banyak", en: "Thank you very much", jp: "ありがとうございます<br>(Arigatou gozaimasu)" },
+    { id: "Terima kasih", en: "Thank you", jp: "ありがとう<br>(Arigatou)" },
+    { id: "Toilet ada di sebelah mana?", en: "Where's the toilet?", jp: "トイレはどこ？<br>(Toire wa doko?)" },
+    { id: "Aku tidak paham", en: "I don't understand", jp: "わかりません<br>(Wakarimasen)" },
+    { id: "Untukmu yang abadi", en: "To your eternity", jp: "不滅のあなたへ<br>(Fumetsu no anata e)" }, 
 ];
