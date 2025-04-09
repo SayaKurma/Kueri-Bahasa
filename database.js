053b3440757ca020caf333e1078128fddd3a4a08
SayaKurma <ridzi1277@gmail.com>
2025-04-09 19:32:55 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,27 @@
+const translations = [
+    {
+        id: "Selamat pagi",
+        en: "Good morning",
+        jp: "おはようございます (Ohayou gozaimasu)",
+    },
+    {
+        id: "Terima kasih banyak",
+        en: "Thank you very much",
+        jp: "ありがとうございます (Arigatou gozaimasu)",
+    },
+    {
+        id: "Terima kasih",
+        en: "Thank you",
+        jp: "ありがとう (Arigatou)",
+    },
+    {
+        id: "Toilet ada di sebelah mana?",
+        en: "Where's the toilet?",
+        jp: "トイレはどこ？ (Toire wa doko?)",
+    },
+    {
+        id: "Aku tidak paham",
+        en: "I don't understand",
+        jp: "わかりません (Wakarimasen)",
+    },
+];
